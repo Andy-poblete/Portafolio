396f6caa1f30c1213f8abbf9c56b9ecbdc42ea1f
Andy-poblete <andy.poblete19@gmail.com>
2024-02-03 19:22:50 -0300

    Se agrega una imagen y un titulo y se eliminan unos textos

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () =>{
+    return (
+        <div className="border-solid border-2 border-white bg-black p-5 text-white text-center">
+            <p>UDD Botcamp 2024</p>
+        </div>
+    )
+}
+
+export default Footer;
